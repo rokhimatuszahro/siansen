68834e8db63623511cca098e258f9ecf710a485f
RokhimatusZahro <rmatuszahro@gmail.com>
2021-07-17 20:30:32 +0700

    commit
    setting

--- analisis/settings.py
+++ analisis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_lu0&*+_3^q2in1!okpj^_o8*wf$z#r)h$p%2g-0et73i%94(7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- analisis/urls.py
+++ analisis/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from analisis_sentimen.views import *
+from django.views.static import serve
+from django.conf.urls.static import url
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('dashboard/', dashboard, name='dashboard'),
     path('upload/', upload, name='upload'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
