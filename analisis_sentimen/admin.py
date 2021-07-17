dad2588c2ae6ecce225a17caf110b90a61cabd97
RokhimatusZahro <rmatuszahro@gmail.com>
2021-07-17 19:16:57 +0700

    commit
    hapus

--- analisis_sentimen/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-@admin.register(Datasets)
-class DatasetsAdmin(admin.ModelAdmin):
-    exclude = ('tweet', 'tanggal_update')
--- analisis_sentimen/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnalisisSentimenConfig(AppConfig):
-    name = 'analisis_sentimen'
